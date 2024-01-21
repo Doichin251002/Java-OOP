9815887b20fbf37ec9779635c17e478ec06ac571
Doichin251002 <dodi_hristov@abv.bg>
2024-01-21 17:05:15 +0200

    Problem solutions in Theme: Encapsulation
    • Keyword this;
    • Access Modifiers;
    • Mutable and Immutable Objects;
    • Keyword final;
    • Validation.

---
+++ 2. Encapsulation/Lab/validationData/Main.java
@@ -0,0 +1,31 @@
+package validationData;
+
+import salaryIncrease.Person;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        List<salaryIncrease.Person> people = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String[] input = reader.readLine().split(" ");
+            try {
+                salaryIncrease.Person person = new salaryIncrease.Person(input[0], input[1], Integer.parseInt(input[2]), Double.parseDouble(input[3]));
+                people.add(person);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        double bonus = Double.parseDouble(reader.readLine());
+        for (Person person : people) {
+            person.increaseSalary(bonus);
+            System.out.println(person);
+        }
+    }
+}
