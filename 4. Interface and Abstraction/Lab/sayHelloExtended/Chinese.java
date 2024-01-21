c372d7abd698eb7022757420d552524971cdd9de
Doichin251002 <dodi_hristov@abv.bg>
2024-01-21 17:34:26 +0200

    Problem solutions in Theme: Problem solutions in Theme: Interfaces and Abstraction

---
+++ 4. Interface and Abstraction/Lab/sayHelloExtended/Chinese.java
@@ -0,0 +1,18 @@
+package sayHelloExtended;
+
+public class Chinese extends BasePerson {
+
+    public Chinese(String name) {
+        super(name);
+    }
+
+    @Override
+    public String getName() {
+        return getName();
+    }
+
+    @Override
+    public String sayHello() {
+        return "Djydjybydjy";
+    }
+}
