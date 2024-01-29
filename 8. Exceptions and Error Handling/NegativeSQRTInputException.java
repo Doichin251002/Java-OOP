a69ec8007a8848ec6b57ee83d3e4aafb4b7bfcf3
Doichin251002 <dodi_hristov@abv.bg>
2024-01-29 23:42:51 +0200

    Problem solutions in Theme: Exceptions and Error Handling
    • Handling Exceptions;
    • Raising (Throwing) Exceptions;
    • Best Practices;
    • Creating Custom Exceptions.

---
+++ 8. Exceptions and Error Handling/NegativeSQRTInputException.java
@@ -0,0 +1,9 @@
+public class NegativeSQRTInputException extends RuntimeException {
+    public NegativeSQRTInputException(String message) {
+        super(message);
+    }
+
+    public NegativeSQRTInputException(String message, Exception cause) {
+        super(message, cause);
+    }
+}
