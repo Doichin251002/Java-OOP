c40cf2a04a714cf6e0f991e06e7baf9e0bd4f1d8
Doichin251002 <dodi_hristov@abv.bg>
2024-02-01 20:11:06 +0200

    Problem solutions in Theme: Unit Testing
    • Unit Testing Basics;
    • Dependancy Injection;
    • Mocking and Mock Objects.

---
+++ 9. Unit Testing/Lab/src/test/java/rpg_lab/DummyTest.java
@@ -0,0 +1,48 @@
+package rpg_lab;
+
+import org.junit.Before;
+import org.junit.Test;
+import rpg_lab.entities.Dummy;
+
+import static org.junit.Assert.*;
+
+public class DummyTest {
+    private static final int HEALTH = 100;
+    private static final int EXPERIENCE = 10;
+    private static final int ATTACK_POINTS = 20;
+
+    private Dummy dummy;
+    private Dummy deadDummy;
+
+    @Before
+    public void setUp() {
+        this.dummy = new Dummy(HEALTH, EXPERIENCE);
+        this.deadDummy = new Dummy(0, EXPERIENCE);
+    }
+
+    @Test
+    public void testDecreaseHealthInAttacked() {
+        this.dummy.takeAttack(ATTACK_POINTS);
+
+        int expectedHealth = HEALTH - ATTACK_POINTS;
+
+        assertEquals(expectedHealth, this.dummy.getHealth());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testTokenAttackThrowsAtDeadDummy() {
+        this.deadDummy.takeAttack(ATTACK_POINTS);
+    }
+
+    @Test
+    public void testGiveExperienceAtDeadDummy() {
+        int actualExperience = this.deadDummy.giveExperience();
+
+        assertEquals(EXPERIENCE, actualExperience);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testGivenExperienceThrowsAtAliveDummy() {
+        this.dummy.giveExperience();
+    }
+}
